63e6a3ba27aa3c27d4b2a624b55b373d7868fe49
drawingathome <shjang@drawingathome.co.kr>
2026-06-03 15:39:10 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: { outDir: 'dist' }
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dah-dashboard.html')
+      }
+    }
+  }
 })
